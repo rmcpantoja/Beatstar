c3eef5aff5074c54a11983e1618aafc0b5cd9b13
ogomez92 <ogomez.s92@gmail.com>
2021-07-21 21:36:55 +0200

    get electron and webpack working together

--- webpack.renderer.config.js
+++ webpack.renderer.config.js
@@ -1,33 +1,9 @@
-const rules = require('./webpack.rules');
-const { name } = require('./package.json');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
-const path = require('path');
-
-rules.push({
-  test: /\.css$/,
-  use: [{ loader: 'style-loader' }, { loader: 'css-loader' }],
-});
-
-module.exports = {
-  // Put your normal webpack config below here
+module.exports = function(env, argv) {
+  const config = {
   target: 'electron-renderer',
-  devtool: 'source-map',
-  module: {
-    rules,
-  },
-  plugins: [
-    new CopyWebpackPlugin({
-      patterns: [
-        {
-          from: path.resolve(__dirname, 'sounds'),
-          to: path.resolve(__dirname, '.webpack/renderer/sounds')
-        },
-        {
-          from: path.resolve(__dirname, 'documentation'),
-          to: path.resolve(__dirname, '.webpack/renderer')
-        },
-      ],
-    })
-  ]
-
+  entry: './src/main.js',
+  output: './bundle.js',
+//  devtool: 'source-map',
 };
+return config;
+}
